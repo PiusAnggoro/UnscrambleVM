d52b3de090fbc36a3e492c5511238b022db4a786
Pius Anggoro <your@email.addresspiusanggoro@gmail.com>
2022-09-22 08:40:10 +0700

    Initial commit

---
+++ app/src/main/java/id/piusanggoro/unscramble/ui/ListofWords.kt
@@ -0,0 +1,32 @@
+package id.piusanggoro.unscramble.ui
+
+const val MAX_NO_OF_WORDS = 4
+const val SCORE_INCREASE = 25
+
+val allWordsList: List<String> =
+    listOf("alfa",
+        "bravo",
+        "charlie",
+        "delta",
+        "echo",
+        "foxtrot",
+        "golf",
+        "hotel",
+        "india",
+        "juliet",
+        "kilo",
+        "lima",
+        "mike",
+        "november",
+        "oscar",
+        "papa",
+        "quebec",
+        "romeo",
+        "sierra",
+        "tango",
+        "uniform",
+        "victor",
+        "whiskey",
+        "xray",
+        "yankee",
+        "zulu")
